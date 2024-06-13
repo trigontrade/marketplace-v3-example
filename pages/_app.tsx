698ae737abd7327ffa9e67baf7990db845cb4ce2
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-06-13 07:58:26 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
+import { defineChain } from "thirdweb"
 import { Navbar } from "../components/Navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const defineChain = "7171";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
